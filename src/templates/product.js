08d293bfc220d6b03eea9f00b7f3b1322d203870
Ahmad <ihsanashi@gmail.com>
2021-03-02 09:05:16 +0800

    add similar items to product template

--- src/templates/product.js
+++ src/templates/product.js
@@ -9,11 +9,14 @@ import ProductAccordion from '../components/product/Accordion';
 import ProductAccordionItem from '../components/product/AccordionItem';
 
 import '../styles/accordion.css';
+import SingleProduct from '../components/product/Single';
 
 const ProductPage = (props) => {
   const { data, errors } = props;
-  const product = data && data.product;
+  const product = (data || {}).product;
   const categorySlug = product.category.slug.current;
+  const similarItems = (data || {}).similarItems;
+  const similarItemsEdges = similarItems.edges;
 
   const [variantName, setVariantName] = useState('');
   const [mainImageIndex, setMainImageIndex] = useState(0);
@@ -51,6 +54,9 @@ const ProductPage = (props) => {
               <small className='text-gray-600'> {product.title}</small>
             </div>
           </section>
+          {/* breadcrumbs */}
+
+          {/* product details */}
           <section
             className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-20
           '
@@ -74,7 +80,7 @@ const ProductPage = (props) => {
               </div>
             </div>
             <div>
-              <h2 className='font-semibold text-gray-900 text-4xl'>
+              <h2 className='font-semibold text-gray-900 text-3xl lg:text-4xl'>
                 {product.title}
               </h2>
               <div className='mt-5'>
@@ -171,7 +177,32 @@ const ProductPage = (props) => {
               </div>
             </div>
           </section>
+          {/* product details */}
         </Container>
+        {/* similar items */}
+        <section className='bg-mountbattenPink-50 py-12'>
+          <Container>
+            <h3 className='text-xl text-gray-800 font-medium mb-5'>
+              You might also be interested in these items
+            </h3>
+            <div
+              className='grid grid-cols-4 gap-4
+          '
+            >
+              {similarItemsEdges.map((item) => (
+                <SingleProduct
+                  key={item.node._id}
+                  link={`/shop/category/${item.node.category.slug.current}/${item.node.slug.current}`}
+                  _id={item.node._id}
+                  imageSrc={item.node.media[0].asset.fluid}
+                  title={item.node.title}
+                  lowestPrice={item.node.variants[0].price}
+                />
+              ))}
+            </div>
+          </Container>
+        </section>
+        {/* similar items */}
       </Layout>
     </>
   );
@@ -180,8 +211,16 @@ const ProductPage = (props) => {
 export default ProductPage;
 
 export const query = graphql`
-  query Products($id: String, $slug: String) {
-    product: sanityProduct(id: { eq: $id }, slug: { current: { eq: $slug } }) {
+  query productAndSimilarItems(
+    $_id: String
+    $slug: String
+    $categoryTitle: String
+  ) {
+    product: sanityProduct(
+      _id: { eq: $_id }
+      slug: { current: { eq: $slug } }
+      category: { title: { eq: $categoryTitle } }
+    ) {
       _id
       title
       category {
@@ -191,12 +230,6 @@ export const query = graphql`
         }
       }
       _rawDescription
-      description {
-        _rawChildren
-        children {
-          text
-        }
-      }
       slug {
         current
       }
@@ -215,6 +248,8 @@ export const query = graphql`
         }
       }
       ingredients
+      careInstructions
+      allergens
       includedItems {
         _id
         details
@@ -228,8 +263,43 @@ export const query = graphql`
           }
         }
       }
-      careInstructions
-      allergens
+    }
+    similarItems: allSanityProduct(
+      filter: {
+        category: { title: { eq: $categoryTitle } }
+        _id: { ne: $_id }
+        slug: { current: { ne: $slug } }
+      }
+      sort: { fields: title, order: ASC }
+      limit: 4
+    ) {
+      edges {
+        node {
+          _id
+          title
+          variants {
+            name
+            price
+            _key
+          }
+          slug {
+            current
+          }
+          media {
+            asset {
+              fluid(maxHeight: 400, maxWidth: 400) {
+                ...GatsbySanityImageFluid
+              }
+            }
+          }
+          category {
+            title
+            slug {
+              current
+            }
+          }
+        }
+      }
     }
   }
 `;
